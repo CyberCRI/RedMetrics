21fd072ab985904f0a681f0135e3b979e3f1ef70
Mikael Couzic <mikaelcouzic@gmail.com>
2014-09-12 12:21:56 +0200

    Put DB user credentials in separate file

--- src/main/java/org/cri/redmetrics/db/Db.java
+++ src/main/java/org/cri/redmetrics/db/Db.java
@@ -8,12 +8,10 @@ import java.sql.SQLException;
 public class Db {
 
     public static final String URL = "jdbc:postgresql://localhost:5432/redmetrics";
-    public static final String USERNAME = "DB_USER_NAME";
-    public static final String PASSWORD = "DB_USER_PASSWORD";
     public static final Class[] ENTITY_TYPES = {Address.class, Event.class, Snapshot.class, Game.class, Group.class, Player.class};
 
     public static JdbcConnectionSource newConnectionSource() throws SQLException {
-        return new JdbcConnectionSource(URL, USERNAME, PASSWORD);
+        return new JdbcConnectionSource(URL, DbUser.NAME, DbUser.PASSWORD);
     }
 
 }
---
+++ src/main/java/org/cri/redmetrics/db/DbUser.java
@@ -0,0 +1,8 @@
+package org.cri.redmetrics.db;
+
+public class DbUser {
+
+    public static final String NAME = "DB_USER_NAME";
+    public static final String PASSWORD = "DB_USER_PASSWORD";
+
+}
