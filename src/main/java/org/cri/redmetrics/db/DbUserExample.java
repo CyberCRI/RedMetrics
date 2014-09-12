bdbfb1fb88646c88d3d89db553f5a609885caf71
Mikael Couzic <mikaelcouzic@gmail.com>
2014-09-12 12:28:51 +0200

    Put DB user credentials in separate file

--- src/main/java/org/cri/redmetrics/db/DbUserExample.java
+++ src/main/java/org/cri/redmetrics/db/DbUserExample.java
@@ -1,6 +1,6 @@
 package org.cri.redmetrics.db;
 
-public class DbUser {
+public class DbUserExample {
 
     public static final String NAME = "DB_USER_NAME";
     public static final String PASSWORD = "DB_USER_PASSWORD";
