b7a114127e4d0cc89574444df3108cb0fa9e543e
Mikael Couzic <mikaelcouzic@gmail.com>
2014-09-12 12:19:05 +0200

    Production-compatible code

--- src/main/java/org/cri/redmetrics/db/Db.java
+++ src/main/java/org/cri/redmetrics/db/Db.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 
 public class Db {
 
-    public static final String URL = "jdbc:postgresql://localhost:5432/test";
-    public static final String USERNAME = "postgres";
-    public static final String PASSWORD = "admin";
+    public static final String URL = "jdbc:postgresql://localhost:5432/redmetrics";
+    public static final String USERNAME = "DB_USER_NAME";
+    public static final String PASSWORD = "DB_USER_PASSWORD";
     public static final Class[] ENTITY_TYPES = {Address.class, Event.class, Snapshot.class, Game.class, Group.class, Player.class};
 
     public static JdbcConnectionSource newConnectionSource() throws SQLException {
