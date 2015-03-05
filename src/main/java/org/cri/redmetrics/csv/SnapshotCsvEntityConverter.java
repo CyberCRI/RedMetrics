671b779147278e6a8dca7f7d057857a2f888ad16
Jesse Himmelstein <jesse@heavenstone.net>
2015-03-05 12:12:12 +0100

    Unpacked custom data for snapshots csv

--- src/main/java/org/cri/redmetrics/csv/SnapshotCsvEntityConverter.java
+++ src/main/java/org/cri/redmetrics/csv/SnapshotCsvEntityConverter.java
@@ -8,6 +8,7 @@ import org.cri.redmetrics.model.Snapshot;
 import org.cri.redmetrics.util.DateFormatter;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by himmelattack on 12/02/15.
@@ -22,7 +23,7 @@ public class SnapshotCsvEntityConverter implements CsvEntityConverter<Snapshot> 
 
     @Override
     public void write(CSVWriter csvWriter, List<Snapshot> models) {
-        csvWriter.writeNext(new String[]{
+        String[] staticColumnNames = {
                 "id",
                 "serverTime",
                 "userTime",
@@ -34,15 +35,25 @@ public class SnapshotCsvEntityConverter implements CsvEntityConverter<Snapshot> 
                 "playerGender",
                 "playerExternalId",
                 "playerCustomData",
-                "section",
-                "customData" });
+                "section"
+        };
 
-        for(Snapshot model : models) {
+        CsvHelper.UnpackedCustomData unpackedCustomData = CsvHelper.unpackCustomData(models);
+
+        String[] customDataColumnNames = unpackedCustomData.columnNames.toArray(new String[0]);
+        String[] allColumnNames = CsvHelper.concatenateArrays(staticColumnNames, customDataColumnNames);
+
+        csvWriter.writeNext(allColumnNames);
+
+        // Write out rows
+        for(int i = 0; i < models.size(); i++) {
+            Snapshot model = models.get(i);
+
             // We need to include additional fields of the player
             // OPT: does this force a new request each time? if so we should join automatically
             playerDao.refresh(model.getPlayer());
 
-            csvWriter.writeNext(new String[]{
+            String[] staticRowValues = {
                     model.getId().toString(),
                     CsvHelper.formatDate(model.getServerTime()),
                     CsvHelper.formatDate(model.getUserTime()),
@@ -54,9 +65,15 @@ public class SnapshotCsvEntityConverter implements CsvEntityConverter<Snapshot> 
                     CsvHelper.formatGender(model.getPlayer().getGender()),
                     model.getPlayer().getExternalId(),
                     model.getPlayer().getCustomData(),
-                    model.getSections(),
-                    model.getCustomData()
-            });
+                    model.getSections()
+            };
+
+            // Write out the custom data in order, skipping fields with no data
+            Map<String, String> rowValues = unpackedCustomData.rowValues.get(i);
+            String[] customDataRowValues = unpackedCustomData.columnNames.stream().map(columnName -> rowValues.get(columnName)).toArray(String[]::new);
+            String[] allRowValues = CsvHelper.concatenateArrays(staticRowValues, customDataRowValues);
+
+            csvWriter.writeNext(allRowValues);
         }
     }
 }
